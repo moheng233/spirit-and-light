57625dca90a230cfac6be720d6ca6df590e6f334
墨恒 <ylm200322400@163.com>
2024-03-19 16:49:29 +0800

    ✨: 初始化项目并编写脚本解析器

---
+++ src/client/java/site/moheng/spirit_and_light/SpiritAndLightClient.java
@@ -0,0 +1,10 @@
+package site.moheng.spirit_and_light;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpiritAndLightClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/site/moheng/spirit_and_light/SpiritAndLightDataGenerator.java
@@ -0,0 +1,11 @@
+package site.moheng.spirit_and_light;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpiritAndLightDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
